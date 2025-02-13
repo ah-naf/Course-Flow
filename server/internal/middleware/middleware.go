05dddd283eefb0e78e4577d5130139fd619e574c
ah-naf <sheikhahnafshifat@gmail.com>
2025-02-13 23:08:56 +0600

    handle panice

--- server/internal/middleware/middleware.go
+++ server/internal/middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"collab-editor/internal/utils"
+	"log"
 	"net/http"
 )
 
@@ -9,6 +10,12 @@ type apiFunc = func(http.ResponseWriter, *http.Request) error
 
 func ConvertToHandlerFunc(f apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panice: %v\n", r)
+				utils.WriteJSON(w, http.StatusInternalServerError, map[string]string{"error": "Internal server error"})
+			}
+		}()
 		if err := f(w, r); err != nil {
 			code, message := mapErrorToStatus(err)
 			utils.WriteJSON(w, code, map[string]string{"error": message})
