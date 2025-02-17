619d1daa317032773b121a3e273f14a8ad8a2ab6
ah-naf <sheikhahnafshifat@gmail.com>
2025-02-17 22:20:03 +0600

    move register related code inside auth handler

--- server/internal/handlers/auth_handler.go
+++ server/internal/handlers/auth_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"collab-editor/internal/models"
 	"collab-editor/internal/services"
+	"collab-editor/internal/utils"
 	"encoding/json"
 	"net/http"
 )
@@ -15,15 +16,35 @@ func NewAuthHandler(service *services.AuthService) *AuthHandler {
 	return &AuthHandler{Service: service}
 }
 
-func (h *AuthHandler) LogUserIn(w http.ResponseWriter, r *http.Request) error {
+// Handles POST /api/auth/login resquests to login user
+func (h *AuthHandler) LoginHandler(w http.ResponseWriter, r *http.Request) error {
 	var req models.LoginRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		return err
 	}
 
-	_, _, err := h.Service.Login(&req)
+	tokens, err := h.Service.Login(&req)
 	if err != nil {
 		return err
 	}
-	return nil
+	return utils.WriteJSON(w, http.StatusOK, tokens)
+}
+
+// Handles POST /api/auth/register requests to register a new user.
+func (h *AuthHandler) CreateUserHandler(w http.ResponseWriter, r *http.Request) error {
+	var userReq models.UserRequest
+	if err := json.NewDecoder(r.Body).Decode(&userReq); err != nil {
+		return err
+	}
+
+	// convert userReq to model.User
+	if err := h.Service.CreateUser(&userReq); err != nil {
+		return err
+	}
+
+	// Respond with the created user details (excluding password)
+	return utils.WriteJSON(w, http.StatusCreated, map[string]interface{}{
+		"message": "User created successfully",
+		"user":    userReq,
+	})
 }
--- server/internal/handlers/user_handler.go
+++ server/internal/handlers/user_handler.go
@@ -1,10 +1,8 @@
 package handlers
 
 import (
-	"collab-editor/internal/models"
 	"collab-editor/internal/services"
 	"collab-editor/internal/utils"
-	"encoding/json"
 	"net/http"
 )
 
@@ -16,25 +14,6 @@ type UserHandler struct {
 // NewUserHandler creates a new UserHandler.
 func NewUserHandler(service *services.UserService) *UserHandler {
 	return &UserHandler{Service: service}
-}
-
-// CreateUserHandler handles POST /api/users/register requests to create a new user.
-func (h *UserHandler) CreateUserHandler(w http.ResponseWriter, r *http.Request) error {
-	var userReq models.UserRequest
-	if err := json.NewDecoder(r.Body).Decode(&userReq); err != nil {
-		return err
-	}
-
-	// convert userReq to model.User
-	if err := h.Service.CreateUser(&userReq); err != nil {
-		return err
-	}
-
-	// Respond with the created user details (excluding password)
-	return utils.WriteJSON(w, http.StatusCreated, map[string]interface{}{
-		"message": "User created successfully",
-		"user":    userReq,
-	})
 }
 
 // GetUserHandler handle GET /api/users requests to get all the user.
